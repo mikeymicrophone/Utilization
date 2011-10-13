d73e76d568611ed0ddfeb1ecb6326b173051105d
Mike Schwab <mike.schwab@gmail.com>
2011-10-13 10:45:04 -0400

    added :host to prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Utilization::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_mailer.default_url_options = { :host => 'radiant-dawn-2656.herokuapp.com' }
   # Code is not reloaded between requests
   config.cache_classes = true
 
